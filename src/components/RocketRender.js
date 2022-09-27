82b51c796aea6e4ef9715eaab95108076a74803f
GameMaster <businesswitharmando@gmail.com>
2022-09-26 21:09:55 -0500

    Added some components and imgs

--- src/components/RocketRender.js
+++ src/components/RocketRender.js
@@ -1,3 +1,4 @@
+import './rocketRndr.css';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -5,28 +6,19 @@ const RocketsRender = (props) => {
   const dispatch = useDispatch();
   const {
     description,
-    manufacturers,
     mission_id,
     mission_name,
-    payload_ids,
-    twitter,
-    website,
-    wikipedia,
   } = props;
 
   const setID = `${mission_id}`;
 
   return (
     <div className="bks_main_cont" id={setID}>
+      <div className="wikipedia" />
       <div className="books_container">
         <div className="description">{description}</div>
-        <div className="manufacturers">{manufacturers}</div>
         <div className="mission_id">{mission_id}</div>
         <div className="mission_name">{mission_name}</div>
-        <div className="payload_ids">{payload_ids}</div>
-        <div className="twitter">{twitter}</div>
-        <div className="website">{website}</div>
-        <div className="wikipedia">{wikipedia}</div>
       </div>
     </div>
   );
