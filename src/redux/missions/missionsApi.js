80dc90f778b1e94be0b1512398977d306673d8b9
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-28 20:17:03 +0430

    fetch mission api

---
+++ src/redux/missions/missionsApi.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const baseURL = 'https://api.spacexdata.com/v3/missions';
+const initialState = [];
+
+ export const fetchMissions = createAsyncThunk(
+ 'missions/getMission',
+ async () => {
+   const response = await fetch(baseURL);
+   const data = await response.json();
+   const missionInfo = data.map((mission) => ({
+    id: mission.mission_id,
+    name: mission.mission_name,
+    description: mission.description,
+    canceled: false,
+   }));
+   return missionInfo;
+ },
+);
+
+export const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {
+    reserveMission: (state, action) => state.map((mission) => {
+      if (mission.mission_id !== action.payload.mission.mission_id) {
+        return mission;
+      }
+      return { ...mission, reserved: !mission.reserved };
+    }),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.fulfilled, (state, action) => action.payload);
+  },
+});
+
+export const { reserveMission } = missionsSlice.actions;
+export default missionsSlice.reducer;
