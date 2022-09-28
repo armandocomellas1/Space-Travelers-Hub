cdc4d0f2a22794844f67d49c90fd3c99f5da5d32
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-28 20:19:04 +0430

    add mission on navbar

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Link,
   NavLink,
 } from 'react-router-dom';
 import Rockets from './components/Rockets';
+import Missions from './components/Missions';
 
 const Navegation = () => (
   <div className="nav_main_cont">
     <div className="nav_container">
-      <div>
+      <div className="menu_container">
+        <div className="menu_img" />
         <h1>Space Traveler&apos;s Hub</h1>
       </div>
-      <nav>
+      <nav className="nav_menu">
         <NavLink
           to="/rockets"
           style={({ isActive }) => ({
-            color: isActive ? 'red' : 'blue',
-            textDecoration: isActive ? 'underline' : 'none',
+            textDecoration: isActive ? 'underline 2px' : 'none',
           })}
         >
           Rockets
@@ -29,8 +29,7 @@ const Navegation = () => (
         <NavLink
           to="/missions"
           style={({ isActive }) => ({
-            color: isActive ? 'red' : 'blue',
-            textDecoration: isActive ? 'underline' : 'none',
+            textDecoration: isActive ? 'underline 2px' : 'none',
           })}
         >
           Missions
@@ -38,13 +37,15 @@ const Navegation = () => (
         <NavLink
           to="/myprofile"
           style={({ isActive }) => ({
-            color: isActive ? 'red' : 'blue',
-            textDecoration: isActive ? 'underline' : 'none',
+            textDecoration: isActive ? 'underline 2px' : 'none',
           })}
         >
           My Profile
         </NavLink>
       </nav>
+    </div>
+    <div className="line_menu">
+      <hr />
     </div>
   </div>
 );
@@ -55,6 +56,7 @@ function App() {
       <Navegation />
       <Routes>
         <Route path="/*" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
       </Routes>
     </BrowserRouter>
   );
